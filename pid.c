0c3ed8bed69d98940c28c3b7a2963af6696493d0
Deng Qingyu <bitdqy@hotmail.com>
2015-08-04 21:30:31 +0800

    Finish document

---
+++ pid.c
@@ -0,0 +1,18 @@
+//  ===========================================================================
+//
+//	Copyright Deng Qingyu 2015
+//	Lisense: MIT Lisense
+//  Author: Deng Qingyu (bitdqy@hotmail.com)
+//
+//                                  pid.c
+//  ===========================================================================
+//  Implement of PID(Proportion, Integration, Differential) control algorithm.
+//  As the parameters of PID have obvious physical meaning ,PID algorithm is 
+//  widely applied to object control. 
+//
+//  ===========================================================================
+//                             Revise History
+//  V1.0.0    2015-8-4                        First Version
+//
+//  ===========================================================================
+
---
+++ pid.h
@@ -0,0 +1,138 @@
+//  ===========================================================================
+//
+//	Copyright Deng Qingyu 2015
+//	Lisense: MIT Lisense
+//  Author: Deng Qingyu (bitdqy@hotmail.com)
+//
+//                                  pid.h
+//  ===========================================================================
+//  Document of PID algorithm
+//
+//  ===========================================================================
+//                             Revise History
+//  V1.0.0    2015-8-4                        First Version
+//
+//  ===========================================================================
+
+#ifndef PID_H
+#define PID_H
+
+//  ===========================================================================
+
+//  Struct for target object you want to control with PID
+//  For every object, independent instance of this struct shall be created.
+//  WARNING:    All field SHALL NOT be modified DIRECTLY
+typedef struct {
+    double target;                  //  Control target value
+    double output;                  //  PID output control signal
+    
+    double upperlimit;              //  Upper limit of output signal
+    double lowerlimit;              //  Lower limit of output signal
+    
+    double error_k_1;               //  e[k-1]
+    double error_k_2;               //  e[k-2]
+    
+    double kp;                      //  Proportional constant
+    double ti;                      //  Intergal time constant
+    double td;                      //  Differential time constant
+    double sampletime;              //  Sample time
+
+    //  For quick calculation
+    //  SHALL NOT MODIFIED DIRECTLY
+    //  u[k] = u[k-1] + f_k * e[k] + f_k-1 * e[k-1] + f_k-2 * e[k-2]
+    double factorError_k;         
+    double factorError_k_1;  
+    double factorError_k_2;
+           
+    int    statusBit;               //  Save some event such as 
+                                    //  parameter change, which require 
+                                    //  to recalculate the factors
+} PID_t;
+
+//  Mode of PID output limiter
+enum PID_Limiter {
+    inhibit,
+    onlyUpperLimiter,
+    onlyLowerLimiter,
+    both
+};
+
+//  MACROs
+
+//  
+
+//  Initial PID structure
+//  Input:
+//  PID_t       *pPID
+//  WARNING:    To keep the correctess, the function MUST be called before
+//              a PID_t instance is used.
+void InitPID(PID_t *pPID);
+
+//  Set control object
+//  Input:
+//  PID_t       *pPID
+//  double      target
+void SetPIDTarget(PID_t *pPID, double target);
+
+//  Set the proportional constant of PID control
+//  Input:
+//  PID_t       *pPID
+//  double      kp
+void SetPIDKp(PID_t *pPID, double kp);
+
+//  Set the time constant of intergal
+//  Input:
+//  PID_t       *pPID
+//  double      ti
+void SetPIDti(PID_t *pPID, double ti);
+
+//  Set the time constant of differential
+//  Input:
+//  PID_t       *pPID
+//  double      td
+void SetPIDtd(PID_t *pPID, double td);
+
+//  Set the sample time of PID algorithm
+//  Input:
+//  PID_t       *pPID
+//  double      sampleTime
+void SetPIDSampleTime(PID_t *pPID, double sampleTime);
+
+//  Set the output limiter mode
+//  Input:
+//  PID_t       *pPID
+//  PID_Limiter mode    (inhibit, onlyLowerLimiter, onlyUpperLimiter, both)
+void ConfigurePIDLimiter(PID_t *pPID, PID_Limiter mode)
+
+//  Set upper limit. When the output is more then upper limit,
+//  the output would be the limit.
+//  Input:
+//  PID_t       *pPID
+//  double      upperLimit
+void SetPIDUpperLimit(PID_t *pPID, double upperLimit);
+
+//  Set lower limit. When the output is less then lower limit,
+//  the output would be the limit.
+//  Input:
+//  PID_t       *pPID
+//  double      lowerLimit
+void SetPIDLowerLimit(PID_t *pPID, double lowerLimit);
+//void CalculatePIDFactor(PID_t *pPID);
+
+//  Update the PID structure with actual status, which should 
+//  be detected by sensors.
+//  Input:
+//  PID_t       *pPID
+//  double      actual  The actual status of object
+void UpdatePID(PID_t *pPID, double actual);
+
+//  Get PID output control signal
+//  Input:
+//  PID_t       *pPID
+//  Output:
+//  double      the output control signal
+//  WARNING:    MAKE SURE that UpdatePID is called before this function call
+double GetPIDOutput(PID_t *pPID);
+
+//  End of File
+#endif
